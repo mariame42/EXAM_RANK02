4cb9f5e4130e26fdc66d8da7c164bdfdf370b64f
meid <meid@student.42abudhabi.ae>
2024-09-07 09:56:04 +0400

    Update test1.c

--- Level 2/ft_strcmp/test1.c
+++ Level 2/ft_strcmp/test1.c
@@ -1,16 +1,1 @@
-// #include <stdio.h>
-// #include <string.h>
 
-int    ft_strcmp(char *s1, char *s2)
-{
-    if (s1 < s2)
-        return (-1);
-    if (s1 > s2)
-        return (1);
-    return (0);
-}
-// int main(void)
-// {
-//     printf("%d\n", ft_strcmp("1", "2"));
-//     printf("%d", strcmp("1", "2"));
-// }
