b8f7e460f66f4d9fb593be860e9eea5fb21e4116
meid <meid@student.42abudhabi.ae>
2024-10-04 12:37:24 +0000

    1

--- Level 4/rostring/test1.c
+++ Level 4/rostring/test1.c
@@ -5,36 +5,32 @@ int main(int ac, char **av)
     if (ac > 1)
     {
         int i = 0;
-        int j = 1;
-        while (ac > 1)// 4 > 0 true
+        if (ac > 1)// 4 > 0 true
         {
-            while (av[j][i] && (av[j][i] == ' ' || av[j][i] == '\t'))
+            while (av[1][i] && (av[1][i] == ' ' || av[1][i] == '\t'))
                 i++;
             int start = i;
-            while (av[j][i] && (av[j][i] != ' ' && av[j][i] != '\t'))
+            while (av[1][i] && (av[1][i] != ' ' && av[1][i] != '\t'))
                 i++;
             int end = i - 1;
-            while (av[j][i] && (av[j][i] == ' ' || av[j][i] == '\t'))
+            while (av[1][i] && (av[1][i] == ' ' || av[1][i] == '\t'))
                 i++;
-            while (av[j][i])
+            while (av[1][i])
             {
-                while (av[j][i] && (av[j][i] == ' ' || av[j][i] == '\t'))
+                while (av[1][i] && (av[1][i] == ' ' || av[1][i] == '\t'))
                     i++;
-                while (av[j][i] && (av[j][i] != ' ' && av[j][i] != '\t'))
+                while (av[1][i] && (av[1][i] != ' ' && av[1][i] != '\t'))
                 {
-                    write(1, &av[j][i], 1);
+                    write(1, &av[1][i], 1);
                     i++;
                 }
                 write(1, " ", 1);
             }
             while (start <= end)
             {
-                write(1, &av[j][start], 1);
+                write(1, &av[1][start], 1);
                 start++;
             }
-            i = 0;
-            j++;
-            ac--;
         }
     }
     write(1, "\n", 1);
